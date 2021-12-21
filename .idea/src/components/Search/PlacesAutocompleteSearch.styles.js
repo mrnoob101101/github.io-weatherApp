84ffacda67dfdb1b6b42a120a07932f75102f497
mrnoob101101 <mazhor101@ya.ru>
2021-12-21 20:05:02 +0200

    trying to delete .idea

--- .idea/src/components/Search/PlacesAutocompleteSearch.styles.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const StyledSearch = styled.input`
-  width: 600px;
-  font-size: 25px;
-`;
--- .idea/src/store/root.reducer.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from "@reduxjs/toolkit";
-import weatherForecastSlice from "./autocomplete.slice/weatherForecast.slice";
-
-
-export const rootReducer = combineReducers({
-    forecast: weatherForecastSlice,
-})
